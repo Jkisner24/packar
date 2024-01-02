4a78eeccd35eb906ee4c9be39c6578d042c78d05
EderFornero <ederfornero.ef@gmail.com>
2024-01-02 19:46:42 -0300

    done login form

--- components/LoginForm/LoginForm.jsx
+++ components/LoginForm/LoginForm.jsx
@@ -1,48 +1,48 @@
-"use client";
-import React from "react";
+"use client"
+import React from "react"
 
 const LoginForm = () => {
   const onSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+  }
 
   return (
-      <form onSubmit={onSubmit} className="row justify-content-center">
-        <div className="mb-3 col-12 col-md-6 text-left">
-          <label htmlFor="exampleInputEmail1" className="form-label mt-3 px-2">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <label htmlFor="exampleInputPassword1" className="form-label mt-3 px-2">
-            Contraseña
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-          <div className="text-primary text-center mt-5">
-            <span style={{ cursor: "pointer" }}>
-              ¿Has olvidado la contraseña?
-            </span>
-          </div>
+    <form onSubmit={onSubmit} className="row justify-content-center">
+      <div className="mb-3 col-12 col-md-6 text-left">
+        <label htmlFor="exampleInputEmail1" className="form-label mt-3 px-2">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <label htmlFor="exampleInputPassword1" className="form-label mt-3 px-2">
+          Contraseña
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+        <div className="text-primary text-center mt-5">
+          <span style={{ cursor: "pointer" }}>
+            ¿Has olvidado la contraseña?
+          </span>
         </div>
+      </div>
 
-        <div className="mb-3 col-12 row justify-content-center">
-          <button
-            type="submit"
-            className="btn w-auto mx-auto p-3 text-light mt-3"
-            style={{ background: "var(--primary-color)" }}
-          >
-            Iniciar sesión
-          </button>
-        </div>
-      </form>
+      <div className="mb-3 col-12 row justify-content-center">
+        <button
+          type="submit"
+          className="btn w-auto mx-auto p-3 text-light mt-3"
+          style={{ background: "var(--primary-color)" }}
+        >
+          Iniciar sesión
+        </button>
+      </div>
+    </form>
   );
 };
 
--- public/assets/index.js
+++ public/assets/index.js
@@ -1,1 +1,2 @@
 export {default as LeftArrow} from './left-arrow.svg'
+export {default as GoogleLogo} from './google-logo.svg'
