0eee7efb98af0481f1b0cba56fed6b10df8e5530
EderFornero <ederfornero.ef@gmail.com>
2024-01-03 18:11:06 -0300

    undraw svg

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,2 +1,3 @@
 export {default as LeftArrow} from './left-arrow.svg'
 export {default as GoogleLogo} from './google-logo.svg'
+export {default as MobilePhoneNumberSvg} from './mobile-phone-number-svg.svg'
