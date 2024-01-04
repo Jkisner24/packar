f589669fe5cada62a233c7f5b38277c76d84f83a
EderFornero <ederfornero.ef@gmail.com>
2024-01-04 20:56:56 -0300

    navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,35 +1,23 @@
+"use client"
 import Link from "next/link";
 import React from "react"
-import { Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Nav, Navbar } from "react-bootstrap"
+import style from './Navbar.module.css'
 
 export default function App() {
   return (
-    <Navbar collapseOnSelect expand="col" bg="dark" variant="dark" className="px-4">
+    <Navbar collapseOnSelect expand="col" style={{background: 'var(--primary-color)'}} className="px-4">
       <Navbar.Brand>
-        <Link href='/home'>PACKAR</Link>
+        <Link href='/home' className="text-white" style={{textDecoration: 'none'}}>PACKAR</Link>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" color="light" style={{color: 'white', background: 'white'}}/>
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+        <Nav className="mr-auto" >
+          <Nav.Link href="" className={`text-light ${style['scale']}`}>Features</Nav.Link>
+          <Nav.Link href="" className={`text-light ${style['scale']}`}>Pricing</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
+          <Nav.Link href="" className={`text-light ${style['scale']}`}>More deets</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
