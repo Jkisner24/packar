79aaf2297531206643637862eded6e3df4af9b0a
Julian Kisner <julikisner@gmail.com>
2024-01-05 20:52:19 -0300

    comment navbar

--- components/HomeComponent/HomeComponent.jsx
+++ components/HomeComponent/HomeComponent.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import GifComponent from './GifComponent'
+/* import NavBar from '../NavBar/NavBar'
+ */import GifComponent from './GifComponent'
 
 const HomeComponent = () => {
   return (
     <div className='w-100 h-auto'>
-      <NavBar />
-      <GifComponent />
+{/*       <NavBar />
+ */}      <GifComponent />
     </div>
   )
 }
