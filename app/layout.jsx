cf399e7831f6ef5e2c3a07593a512742d17a9196
EderFornero <ederfornero.ef@gmail.com>
2024-01-05 19:21:47 -0300

    monserrat font

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import { Montserrat } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
 import './globals.css'
 import { StoreProvider } from '@/store/StoreProvider'
-import Script from 'next/script'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <StoreProvider>
       <html lang="en">
-        <body className="montserrat">{children}</body>
+        <body className={montserrat.className}>{children}</body>
         <script id='maps' src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`}></script>
      </html>
     </StoreProvider>
