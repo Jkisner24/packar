e67681b8f03125661b90d8ba39318ad491f9ce28
Your Name <you@example.com>
2024-01-05 00:15:28 -0300

    Formulario listo

--- components/Logic/ComponeLogic.jsx
+++ components/Logic/ComponeLogic.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useCallback } from 'react';
 import { useFormik } from 'formik';
+import Image from 'next/image';
 import { Modal, Button, Form } from 'react-bootstrap';
-import { PrismaClient } from '@prisma/client';
 import { Autocomplete, GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { home } from '@/public/assets';
 
 const generateRandomProducts = () => {
   const products = [];
@@ -33,7 +31,7 @@ const EnvioForm = () => {
     },
     onSubmit: async (values) => {
       try {
-        // Inserta una nueva entrada en la tabla Trip
+        // codigo para prisma
         const newTrip = await prisma.trip.create({
           data: {
             title: values.producto,
@@ -81,120 +79,154 @@ const EnvioForm = () => {
   const randomProducts = generateRandomProducts();
 
   return (
-    <div>
-      <Form onSubmit={formik.handleSubmit}>
-        <Form.Group controlId="desde">
-          <Form.Label>Desde</Form.Label>
-          <PlacesAutocomplete
-            value={formik.values.desde}
-            onChange={(address) => formik.setFieldValue('desde', address)}
-            onSelect={(address) => handleLocationSelect(address, 'desde')}
-          >
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-              <div className="my-3 col-12 col-md-6 text-left d-flex flex-column">
-                <input
-                  {...getInputProps({
-                    placeholder: 'Selecciona la ubicación de partida',
-                    className:"mt-1 p-2 w-full rounded-full border",
-                  })}
-                />
-                <div>
-                  {loading && <div>Loading...</div>}
-                  {suggestions.map((suggestion) => (
-                    <div {...getSuggestionItemProps(suggestion)} key={suggestion.placeId}>
-                      {suggestion.description}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-          </PlacesAutocomplete>
-        </Form.Group>
-
-        <Form.Group controlId="hasta">
-          <Form.Label>Hasta</Form.Label>
-          <PlacesAutocomplete
-            value={formik.values.hasta}
-            onChange={(address) => formik.setFieldValue('hasta', address)}
-            onSelect={(address) => handleLocationSelect(address, 'hasta')}
-          >
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-              <div>
-                <input
-                  {...getInputProps({
-                    placeholder: 'Selecciona la ubicación de destino',
-                    className:"mt-1 p-2 w-full rounded-full border",
-                  })}
-                />
-                <div>
-                  {loading && <div>Loading...</div>}
-                  {suggestions.map((suggestion) => (
-                    <div {...getSuggestionItemProps(suggestion)} key={suggestion.placeId}>
-                      {suggestion.description}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-          </PlacesAutocomplete>
-        </Form.Group>
-
-        <Form.Group controlId="fecha">
-          <Form.Label>Fecha del viaje</Form.Label>
-          <Form.Control
-            type="date"
-            placeholder="Selecciona la fecha del viaje"
-            value={formik.values.fecha}
-            onChange={formik.handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="producto">
-          <Form.Label>Producto</Form.Label>
-          <Form.Control
-            as="select"
-            placeholder="Selecciona el producto a transportar"
-            value={formik.values.producto}
-            onChange={formik.handleChange}
-          >
-            <option value="">Selecciona un producto</option>
-            {/* Incluye tus 20 productos aleatorios aquí */}
-          </Form.Control>
-        </Form.Group>
-
-        <Button type="submit">Enviar</Button>
-      </Form>
-
-      {/* Modal para seleccionar la ubicación */}
-      <Modal show={showModal} onHide={handleCloseModal} onEntered={handleOpenModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Selecciona la ubicación</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {showModal && (
-            <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-              <GoogleMap
-                mapContainerStyle={{ width: '100%', height: '400px' }}
-                center={selectedLocation?.latLng || { lat: -34.397, lng: 150.644 }}
-                zoom={8}
-                onLoad={onLoad}
+    <div className="fw-bold fs-6 " style={{ color: 'var(--accent-color)' }}>
+  <div className="relative h-screen flex items-center justify-center">
+    <Image src={home} alt="Fondo de pantalla" className="w-full max-w-md h-auto" width={750} />
+    <div className="container my-5 border border-black p-4 rounded-md text-center shadow" style={{ maxWidth: '800px', borderRadius: '10px' }}>
+      <div className="mt-2 col-12">
+        <div style={{ cursor: 'pointer' }}>
+          <span className="my-10 text-black text-3xl font-bold">
+            ¿Qué quieres enviar?
+          </span>
+              <Form
+                className="my-3 mx-auto col-12 col-md-6 text-center d-flex flex-column"
+                onSubmit={formik.handleSubmit}
               >
-                {mapRef.current && selectedLocation && (
-                  <Marker
-                    position={selectedLocation.latLng}
-                    onClick={() => {
-                      console.log('Marker clicked');
+                <Form.Group controlId="desde">
+                  <PlacesAutocomplete
+                    value={formik.values.desde}
+                    onChange={(address) => formik.setFieldValue('desde', address)}
+                    onSelect={(address) => handleLocationSelect(address, 'desde')}
+                  >
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                      <div className="my-1">
+                        <input
+                          {...getInputProps({
+                            placeholder: 'Desde',
+                            className: 'w-full border-none outline-none bg-transparent mb-2',
+                            style: {
+                              borderBottom: '-1px solid #000',
+                              height: '35px',
+                              width: '100%', 
+                              borderRadius: '4px',
+                              boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                            },
+                          })}
+                        />
+                        <div>
+                          {loading && <div>Loading...</div>}
+                          {suggestions.map((suggestion) => (
+                            <div {...getSuggestionItemProps(suggestion)} key={suggestion.placeId}>
+                              {suggestion.description}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                  </PlacesAutocomplete>
+                </Form.Group>
+
+                <Form.Group controlId="hasta">
+                  <PlacesAutocomplete
+                    value={formik.values.hasta}
+                    onChange={(address) => formik.setFieldValue('hasta', address)}
+                    onSelect={(address) => handleLocationSelect(address, 'hasta')}
+                  >
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                      <div className="my-2">
+                        <input
+                          {...getInputProps({
+                            placeholder: 'Hasta',
+                            className: 'w-full border-none outline-none bg-transparent mb-2',
+                            style: {
+                              borderBottom: '1px solid #000',
+                              height: '35px',
+                              width: '100%', 
+                              borderRadius: '4px',
+                              boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                            },
+                          })}
+                        />
+                        <div>
+                          {loading && <div>Loading...</div>}
+                          {suggestions.map((suggestion) => (
+                            <div {...getSuggestionItemProps(suggestion)} key={suggestion.placeId}>
+                              {suggestion.description}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                  </PlacesAutocomplete>
+                </Form.Group>
+
+                <Form.Group controlId="fecha">
+                  <Form.Control
+                    type="date"
+                    placeholder="Selecciona la fecha del viaje"
+                    value={formik.values.fecha}
+                    onChange={formik.handleChange}
+                    className="w-full border-none outline-none bg-transparent mb-2"
+                    style={{
+                      borderBottom: '1px solid #000',
+                      height: '35px',
+                      borderRadius: '4px',
+                      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
                     }}
                   />
-                )}
-              </GoogleMap>
-            </LoadScript>
-          )}
-        </Modal.Body>
-      </Modal>
+                </Form.Group>
+
+                <Form.Group controlId="producto">
+                  <Form.Control
+                    as="select"
+                    placeholder="Producto"
+                    value={formik.values.producto}
+                    onChange={formik.handleChange}
+                    className="w-full border-none outline-none bg-transparent mb-2"
+                    style={{
+                      borderBottom: '1px solid #000',
+                      height: '35px',
+                      borderRadius: '4px',
+                      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                    }}
+                  >
+                    <option value="">Producto</option>
+                    {randomProducts.map((product) => (
+                      <option key={product} value={product}>
+                        {product}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="buscar">
+                <Button
+             type="submit"
+             className="btn w-full p-3 text-light mt-3"
+             style={{background: '#FE1252',
+             width: '50%', }}
+             onClick={handleOpenModal}
+               >
+            Buscar
+              </Button>
+                </Form.Group>
+              </Form>
+            </div>
+          </div>
+        </div>
+
+       
+        <Modal show={showModal} onHide={handleCloseModal} onEntered={handleOpenModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Loading...</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            
+          </Modal.Body>
+        </Modal>
+      </div>
     </div>
   );
 };
 
 export { EnvioForm };
-
